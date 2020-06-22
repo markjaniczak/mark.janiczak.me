41121524c436557b46194f5f89f9a03ac320df27
Mark Janiczak <mark@janiczak.me>
2020-06-22 22:28:04 +1000

    Debugging hydration issue

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,1 +1,41 @@
 import "./src/styles/global.css"
+import React from 'react'
+
+exports.onRenderBody = ({ setPreBodyComponents  }) => {
+    setPreBodyComponents([
+      React.createElement('script', {
+        dangerouslySetInnerHTML: {
+          __html: `
+            (() => {    
+              window.__onThemeChange = function() {};                
+              function setTheme(newTheme) {                  
+                window.__theme = newTheme;                  
+                preferredTheme = newTheme;                  
+                document.body.className = newTheme;                 
+                window.__onThemeChange(newTheme);                
+              }
+  
+              let preferredTheme
+              try {
+                preferredTheme = localStorage.getItem('theme')
+              } catch (err) {}
+  
+              window.__setPreferredTheme = newTheme => {
+                setTheme(newTheme)
+                try {
+                  localStorage.setItem('theme', newTheme)
+                } catch (err) {}
+              }
+  
+              let darkQuery = window.matchMedia('(prefers-color-scheme: dark)')
+              darkQuery.addListener(e => {
+                window.__setPreferredTheme(e.matches ? 'dark' : 'light')
+              })
+  
+              setTheme(preferredTheme || (darkQuery.matches ? 'dark' : 'light'))
+            })()
+          `,
+        },
+      }),
+    ])
+  }
